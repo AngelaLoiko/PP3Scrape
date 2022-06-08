206802f6d4da577b65a360c3e3883d48f454d231
Angela Loiko <angelaloiko@gmail.com>
2022-06-08 18:48:42 +0200

    done

---
+++ headers.py
@@ -0,0 +1,20 @@
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+    'Cash-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Cookie': 'habr_web_home_feed=/all/; hl=ru; fl=ru; _ga=GA1.2.2067820903.1654597654; _gid=GA1.2.2135179559.1654597654; _ym_uid=1654597656811939241; _ym_d=1654597656; _ym_isad=2',
+    'Host': 'habr.com',
+    'Referer': 'https://habr.com/ru/all/',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0'
+  # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+}
--- main.py
+++ main.py
@@ -1,59 +1,47 @@
+from types import NoneType
 import requests
-from requests_html import HTMLSession
-from bs4 import BeautifulSoup
-import time
+import bs4
+import fake_useragent
 import re
+# from headers import HEADERS
+KEYWORDS = ['дизайн', 'фото', 'web', 'python','костюм', 'финансисты ']
+# KEYWORDS = ['ужас']
+rkeywords = KEYWORDS[0]
+for kw in KEYWORDS[1:]:
+    rkeywords += f'|{kw}'
+print(rkeywords)
 
-def text_to_word(text):
-    pattern= ";|,|\n|\.|\?|\!|\s+"
-    preview = text.lower()
-    preview = re.split(pattern, preview)
-    return set(preview)
+useragent_ = fake_useragent.UserAgent()
+HEADERS = {'user-agent': useragent_.random}
 
-url='https://habr.com/ru/all'  # без этого не делает выдачу контента
-url1='https://habr.com' # без этого кривые ссылки
-DESIRED_HUBS = ['дизайн', 'фото', 'web', 'python','it-эмиграция','базовые' , 'bpmn']
+urlfull='https://habr.com/ru/all'
+url='https://habr.com'
 
-interval = input('Введите интервал проверки (сек):' )
-session = HTMLSession()
-response = session.get(url)
-text = response.text # голый текст страницы
-soup = BeautifulSoup(text, features='html.parser')  # features='lxml')
-
-not_war = True
-while not_war:
-    articles = soup.find_all('article')  # список объектов article выдает find_all -
-    for article in articles:
-        hubs = article.find_all(class_="tm-article-snippet__hubs-item")
-        # class="article-formatted-body article-formatted-body article-formatted-body_version-1" version меняются 1 2
-        preview1 = article.find(class_="article-formatted-body article-formatted-body article-formatted-body_version-1")
-        preview2 = article.find(class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
-        if preview1:
-            preview = preview1
-        elif preview2:
-            preview = preview2
-        else:
-            print('что то пошло не так')
-        preview = text_to_word(preview.text)
-        hubs = set(hub.text.strip() for hub in hubs) # set избавляемся от дублей  strip избавляемся от пробелов
-        href = article.find(class_="tm-article-snippet__title-link").attrs['href']
-        date = article.find('time').attrs['title']
-        date1 = article.find('time').text
-        link = url1 + href
-        title = head = article.find(class_='tm-article-snippet__title-link').text
-        # или так
-        title = article.find('h2').find('span').text  # контейнер h2 статьи и в нем найти span
-        title_set = text_to_word(title)
-
-        DESIRED_HUBS  = set(DESIRED_HUBS)
-        intersection = preview.intersection(DESIRED_HUBS)
-        intersection2 = title_set.intersection(DESIRED_HUBS)
-
-        for hub in hubs:
-            if hub.lower() in DESIRED_HUBS or intersection or intersection2:
-                result= f'{date}, {date1}, Статья - {title} доступна по ссылке- {link}'
-                print(result)
-                # новый запрос
-                response = requests.get(link) #, headers=HEADERS)#)  конкретная статья
-                break
-    time.sleep(int(interval))
+if __name__ == '__main__':    
+    pageexist = True
+    while pageexist:
+        try:
+            response = requests.get(urlfull, headers=HEADERS)
+            response.raise_for_status()
+        except Exception:
+            print('Ошибка при открытии страницы')
+            break
+        text = response.text
+        soup = bs4.BeautifulSoup(text, 'html.parser')
+        articles = soup.find_all('article')
+        for article in articles:
+            preview = article.find('div', class_="article-formatted-body article-formatted-body article-formatted-body_version-2")
+            if (type(preview) != NoneType) and (re.search(rkeywords, preview.text.lower())):
+                # print(preview.text.lower())
+                # headarticle = article.find_all('span', class_=None)[:-1]
+                date = article.find_all('time')[0]['datetime'][:10]
+                title = article.find_all(class_='tm-article-snippet__title-link')[0].find_all('span')[0].text
+                link = url + article.find_all(class_='tm-article-snippet__title-link')[0]['href']
+                print(f'{date} - {title} - {link}')
+        try:
+            urlfull = url + soup.find(id="pagination-next-page")['href']
+            print(urlfull)
+        except KeyError:
+            print('Больше нет страниц')
+            # pageexist = False
+            break
